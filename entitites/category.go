dfef17a4d8d7c4c72815b809193c1f7c233f009e
aurellieandra <diandra.azra@gmail.com>
2024-01-22 20:08:41 +0700

    adding features for categories

---
+++ entitites/category.go
@@ -0,0 +1,10 @@
+package entitites
+
+import "time"
+
+type Category struct {
+	Id        uint
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"log"
 	"net/http"
 	"simple-go-web/config"
+	homecontroller "simple-go-web/controllers/HomeController"
+	"simple-go-web/controllers/categorycontroller"
 )
 
 func main() {
 	config.ConnectDB()
+
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	// categories
+	http.HandleFunc("/categories/", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
 
 	log.Println("Server running on PORT 8000")
 	http.ListenAndServe(":8080", nil)
